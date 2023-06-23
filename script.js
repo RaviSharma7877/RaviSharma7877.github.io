2a4b823d0eff19829929b65f829f4dd6f5240901
RaviSharma7877 <sraa7877@gmail.com>
2023-06-23 19:36:05 +0530

    done

---
+++ script.js
@@ -0,0 +1,25 @@
+window.addEventListener('scroll', function() {
+    var navMenu = document.getElementById('nav-menu');
+    var sticky = navMenu.offsetTop;
+  
+    if (window.pageYOffset >= sticky) {
+      navMenu.classList.add('sticky');
+    } else {
+      navMenu.classList.remove('sticky');
+    }
+  });
+  // Get all the navbar links
+var navLinks = document.querySelectorAll('#nav-menu ul li a');
+
+// Loop through each link and attach a click event listener
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener('click', function() {
+    // Remove the "active" class from all links
+    for (var j = 0; j < navLinks.length; j++) {
+      navLinks[j].classList.remove('active');
+    }
+
+    // Add the "active" class to the clicked link
+    this.classList.add('active');
+  });
+}
